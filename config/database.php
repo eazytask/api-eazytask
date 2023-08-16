d63da37b35731c5bc2e3365471dc0b1fab0b0320
Heri Hermawan <herihermawan@Heris-MacBook-Air.local>
2023-08-16 17:24:59 +0700

    updateStrict

--- config/database.php
+++ config/database.php
@@ -67,7 +67,7 @@ return [
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
             'modes' => [
-                 //'ONLY_FULL_GROUP_BY', // Disable this to allow grouping by one column
+                 'ONLY_FULL_GROUP_BY', // Disable this to allow grouping by one column
                  'STRICT_TRANS_TABLES',
                  'NO_ZERO_IN_DATE',
                  'NO_ZERO_DATE',
