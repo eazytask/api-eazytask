007e56cf774c3e879f5108a6324da69fed56fab4
Heri Hermawan <herihermawan@Heris-MacBook-Air.local>
2023-09-26 01:43:20 +0700

    updateEmailUser

--- resources/views/emails/user-password.blade.php
+++ resources/views/emails/user-password.blade.php
@@ -4,14 +4,4 @@
     Congratulations!
     Your account has been setup and you are ready to explore Eazytask features from
     {{ strtoupper($company) }}.
-
-    Please don't share your credentials with anyone and after login change your password. Your
-    account temporary login credential below-
-
-    Thank you for using our application!
-    If you need any further assistance, please contact our support team
-
-    ---
-    Thanks,<br>
-    {{ config('app.name') }}
 @endcomponent
