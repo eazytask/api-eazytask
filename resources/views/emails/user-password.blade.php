58ad6bebffe5d68fc7a22b026aa49c56baf54f59
Heri Hermawan <herihermawan@Heris-MacBook-Air.local>
2023-09-26 01:41:36 +0700

    updateEmailUser

--- resources/views/emails/user-password.blade.php
+++ resources/views/emails/user-password.blade.php
@@ -8,10 +8,6 @@
     Please don't share your credentials with anyone and after login change your password. Your
     account temporary login credential below-
 
-    @component('mail::button', ['url' => 'https://eazytask.au'])
-        Login Now
-    @endcomponent
-
     Thank you for using our application!
     If you need any further assistance, please contact our support team
 
