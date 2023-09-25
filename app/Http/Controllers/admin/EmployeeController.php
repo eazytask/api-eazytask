6a321b9d34b2cb3553d2e13aede1e9e6da54dfaf
Heri Hermawan <herihermawan@Heris-MacBook-Air.local>
2023-09-26 01:07:07 +0700

    addMailUserCredential

--- app/Http/Controllers/admin/EmployeeController.php
+++ app/Http/Controllers/admin/EmployeeController.php
@@ -18,6 +18,8 @@ use Hash;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
 use Image;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\UserCredential as MailUserCredential;
 
 class EmployeeController extends Controller
 {
@@ -88,7 +90,9 @@ class EmployeeController extends Controller
                 $user->save();
                 $GLOBALS['data'] = $user;
                 try {
-                    $mail = $GLOBALS['data']->notify(new UserCredential($email_data));
+                    $mail = Mail::to($GLOBALS['data']->email)->send(new MailUserCredential($email_data));
+                    
+                    // $mail = $GLOBALS['data']->notify(new UserCredential($email_data));
                 } catch (\Exception $e) {
                     // $GLOBALS['data']->delete();
                     send_response(false, 'validation error!', ['email' => "this email is incorrect."], 400);
---
+++ app/Mail/UserCredential.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class ForgetPassword extends Mailable
+{
+    use SerializesModels; //Queueable, 
+    protected $name;
+    protected $email;
+    protected $password;
+    protected $company;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($email_data)
+    {
+        $this->email=$email_data['email'];
+        $this->name=$email_data['name'];
+        $this->password=$email_data['password'];
+        $this->company=$email_data['company'];
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->subject('Account Credentials')
+        ->from('admin@eazytask.au', 'Eazytask')
+        ->view('emails.user-password', ['name' => $notifiable->name,'email' => $notifiable->email,'user_password' => $this->password,'company'=>$this->company]);
+    }
+}
