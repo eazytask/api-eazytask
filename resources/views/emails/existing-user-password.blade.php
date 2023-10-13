b31d2340f1a1a8fe8f72fbabef5e8d0523a591d4
Heri Hermawan <herihermawan@Heris-MacBook-Air.local>
2023-10-13 14:57:29 +0700

    updateMail

--- resources/views/emails/existing-user-password.blade.php
+++ resources/views/emails/existing-user-password.blade.php
@@ -1,18 +1,25 @@
 @component('mail::message')
-# Dear {{ ucwords($name) }},
+    # Dear {{ ucwords($name) }},
 
-Congratulations!
-Your account has been setup and you are ready to explore Eazytask features from
-{{ strtoupper($company) }}.
+    Congratulations!
+    Your account has been setup and you are ready to explore Eazytask features from
+    {{ strtoupper($company) }}.
 
-@component('mail::button', ['url' => 'https://eazytask.au'])
-    Login Now
-@endcomponent
+    @component('mail::button', ['url' => 'https://eazytask.au'])
+        Login Now
+    @endcomponent
 
-Thank you for using our application!
-If you need any further assistance, please contact our support team
+    @component('mail::panel')
+        [![Download on the App
+        Store](https://w7.pngwing.com/pngs/270/658/png-transparent-app-store-apple-google-play-apple-text-logo-mobile-phones.png)](https://apps.apple.com/id/app/eazytask/id1642332032)
+        [![Get it on Google
+        Play](https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png)](https://play.google.com/store/apps/details?id=com.ni.Easytask)
+    @endcomponent
 
----
-Thanks,<br>
-{{ config('app.name') }}
+    Thank you for using our application!
+    If you need any further assistance, please contact our support team
+
+    ---
+    Thanks,<br>
+    {{ config('app.name') }}
 @endcomponent
--- resources/views/emails/user-password.blade.php
+++ resources/views/emails/user-password.blade.php
@@ -1,24 +1,31 @@
 @component('mail::message')
-# Dear {{ ucwords($name) }},
+    # Dear {{ ucwords($name) }},
 
-Congratulations!
-Your account has been setup and you are ready to explore Eazytask features from
-{{ strtoupper($company) }}.
+    Congratulations!
+    Your account has been setup and you are ready to explore Eazytask features from
+    {{ strtoupper($company) }}.
 
-Please don't share your credentials with anyone and after login change your password. Your
-account temporary login credential below-
+    Please don't share your credentials with anyone and after login change your password. Your
+    account temporary login credential below-
 
-User Email: {{ $email }}
-User Password: {{ $user_password }}
+    User Email: {{ $email }}
+    User Password: {{ $user_password }}
 
-@component('mail::button', ['url' => 'https://eazytask.au'])
-    Login Now
-@endcomponent
+    @component('mail::button', ['url' => 'https://eazytask.au'])
+        Login Now
+    @endcomponent
 
-Thank you for using our application!
-If you need any further assistance, please contact our support team
+    @component('mail::panel')
+        [![Download on the App
+        Store](https://w7.pngwing.com/pngs/270/658/png-transparent-app-store-apple-google-play-apple-text-logo-mobile-phones.png)](https://apps.apple.com/id/app/eazytask/id1642332032)
+        [![Get it on Google
+        Play](https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png)](https://play.google.com/store/apps/details?id=com.ni.Easytask)
+    @endcomponent
 
----
-Thanks,<br>
-{{ config('app.name') }}
+    Thank you for using our application!
+    If you need any further assistance, please contact our support team
+
+    ---
+    Thanks,<br>
+    {{ config('app.name') }}
 @endcomponent
