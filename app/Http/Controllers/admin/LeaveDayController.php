29387bf38fd31d04f2570e61bfb1f61e112e5e78
Heri Hermawan <herihermawan@Heris-MacBook-Air.local>
2023-11-16 00:00:23 +0700

    update

--- app/Http/Controllers/admin/LeaveDayController.php
+++ app/Http/Controllers/admin/LeaveDayController.php
@@ -9,6 +9,7 @@ use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
+use DB;
 
 class LeaveDayController extends Controller
 {
