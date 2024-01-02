c8e4ec5f764d1b6e9b8c1088e127572204397ec0
Heri Hermawan <herihermawan@Heris-MacBook-Air.local>
2024-01-02 13:44:19 +0700

    updateSignOut

--- app/Http/Controllers/user/SignInController.php
+++ app/Http/Controllers/user/SignInController.php
@@ -38,7 +38,7 @@ class SignInController extends Controller
             //     $q->where('shift_end', '>', Carbon::now());
             // });
         })->where(function ($q) {
-            $q->where('roaster_date', Carbon::now()->format("Y-m-d"));
+            // $q->where('roaster_date', Carbon::now()->format("Y-m-d"));
             // $q->orWhere(function ($q) {
             //     $q->where('roaster_date', Carbon::now()->subDay()->format("Y-m-d"));
                 // $q->where('shift_end', '>', Carbon::now()->format("Y-m-d"));
